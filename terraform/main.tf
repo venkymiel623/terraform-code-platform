f4abd541186c08677c3de5181564eeda25ffcc4d
root <root@ip-172-31-14-45.ap-south-1.compute.internal>
2024-04-20 15:52:31 +0000

    adding terraform main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,79 @@
+# main.tf
+
+# Define AWS provider
+provider "aws" {
+  region = "us-west-2"
+}
+
+# Create VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+}
+
+# Create subnets
+resource "aws_subnet" "subnet_a" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-west-2a"
+}
+
+resource "aws_subnet" "subnet_b" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-west-2b"
+}
+
+# Create security group
+resource "aws_security_group" "eks_sg" {
+  vpc_id = aws_vpc.my_vpc.id
+  name   = "eks_sg"
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create EKS cluster
+resource "aws_eks_cluster" "my_cluster" {
+  name            = "my-cluster"
+  role_arn        = aws_iam_role.eks_role.arn
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.subnet_a.id,
+      aws_subnet.subnet_b.id
+    ]
+    security_group_ids = [aws_security_group.eks_sg.id]
+    endpoint_private_access = false
+    endpoint_public_access = true
+  }
+}
+
+# IAM role for EKS
+resource "aws_iam_role" "eks_role" {
+  name = "eks-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+# IAM role policy attachment
+resource "aws_iam_role_policy_attachment" "eks_policy_attachment" {
+  role       = aws_iam_role.eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
